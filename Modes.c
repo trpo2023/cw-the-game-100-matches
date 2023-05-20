5a16c4b57aa549a5a194a74bdd4bb31fd8859f5e
Polinakovr <polinakovrigina129@gmail.com>
2023-05-20 17:36:38 +0700

    Create difficulty level

---
+++ Modes.c
@@ -0,0 +1,26 @@
+#include "Modes.h"
+#include <stdlib.h>
+#include <time.h>
+int calculate_easy(int match) 
+{
+    int take = 0;
+    srand(time(NULL));
+    if (match <= 10) {
+        take = match;
+    } else {
+        take = rand() % 10 + 1;
+    }
+    return take;
+}
+
+int calculate_normal(int match)
+{
+    int take = 0;
+    srand(time(NULL));
+    if (match % 11 != 0) {
+        take = match % 11;
+    } else {
+        take = rand() % 10 + 1;
+    }
+    return take;
+}
---
+++ Modes.h
@@ -0,0 +1,7 @@
+#ifndef Modes_h
+#define Modes_h
+
+int calculate_easy(int match);
+int calculate_normal(int match);
+
+#endif
